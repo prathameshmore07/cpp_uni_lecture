cf8bf6064e5772a64b125af59c79e246e9c229ac
Prathamesh More <prathamesh4244@gmail.com>
2026-01-07 19:30:06 +0530

    add lecture 3

---
+++ lecture 3/vowel_switch.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cout << "Enter a character: ";
+    cin >> ch;
+
+    switch (ch)
+    {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    case 'A':
+    case 'E':
+    case 'I':
+    case 'O':
+    case 'U':
+        cout << "vowel";
+        break;
+
+    default:
+        cout << "not a vowel";
+    }
+
+    return 0;
+}
+
